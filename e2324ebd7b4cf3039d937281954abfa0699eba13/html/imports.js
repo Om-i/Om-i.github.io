401d9fa4da6db8458b3faf94e7a1511bfbd3da2d
Om-i <115377488+Om-i@users.noreply.github.com>
2023-03-09 11:30:54 +0000

    comments

--- e2324ebd7b4cf3039d937281954abfa0699eba13/html/imports.js
+++ e2324ebd7b4cf3039d937281954abfa0699eba13/html/imports.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
-  $('header').load('html/imports.html header');
+$(document).ready(function() { // load the function as soon as the page is opened
+  $('header').load('html/imports.html header'); // replace the header tag with the one from an external html file
   $('footer').load('html/imports.html footer');
 });
