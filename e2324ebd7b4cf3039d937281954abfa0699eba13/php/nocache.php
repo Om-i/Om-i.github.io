57d50bfabafa12bba94a31ce2d0cc6dd753bd9f4
Om-i <115377488+Om-i@users.noreply.github.com>
2023-03-20 11:34:48 +0000

    Database creation in absence of one No cache development setting Fixed form shrinking on input

---
+++ e2324ebd7b4cf3039d937281954abfa0699eba13/php/nocache.php
@@ -0,0 +1,6 @@
+<?php
+echo '<meta http-equiv="cache-control" content="no-cache, must-revalidate, post-check=0, pre-check=0" />';
+echo '<meta http-equiv="cache-control" content="max-age=0" />';
+echo '<meta http-equiv="expires" content="0" />';
+echo '<meta http-equiv="expires" content="Sat, 01 Jan 2000 12:00:00 GMT" />';
+echo '<meta http-equiv="pragma" content="no-cache" />';
--- e2324ebd7b4cf3039d937281954abfa0699eba13/php/post2sql.php
+++ e2324ebd7b4cf3039d937281954abfa0699eba13/php/post2sql.php
@@ -1,26 +1,32 @@
 <?php
 
-$name  = filter_input(INPUT_POST, 'name');  // $_POST['name'];
-$email = filter_input(INPUT_POST, 'email'); 
-$date  = filter_input(INPUT_POST, 'date');  
-$title  = filter_input(INPUT_POST, 'title');  
+$name  = filter_input(INPUT_POST, 'name'); // $_POST['name'];
+$email = filter_input(INPUT_POST, 'email');
+$title = filter_input(INPUT_POST, 'title');
+$date  = filter_input(INPUT_POST, 'date');
 
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "moviewebsite";
+//$dbname     = "moviewebsite";
 
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // makes mysql errors visible as php exceptions
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn -> connect_error) {
-    die("Connection failed: " . $conn -> connect_error);
-}
+$conn = new mysqli($servername, $username, $password);//, $dbname);
 // sql instruction
-$sql = "INSERT INTO booking VALUES (NULL, '$name', '$email', '$date', '$title')";
+$sql = "CREATE DATABASE IF NOT EXISTS `wdCA2023`;
+USE `wdCA2023`;
+CREATE TABLE IF NOT EXISTS `booking` (
+`bookingId` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(255),
+`email` VARCHAR(255),
+`title` VARCHAR(255),
+`date` DATE);
+INSERT INTO `booking` VALUES (NULL, '$name', '$email', '$title', '$date');";
 
 //$conn -> query($sql); returns true when successful
-if ($conn -> query($sql) === TRUE) {
+if ($conn -> multi_query($sql) === TRUE) { // query() produces syntax error when multiple commands are issued. multi_query() must be used
     echo "<script>";
     echo "alert('Congratulations, $name. Booking Successful!');";
     echo "location.replace('../index.html');"; // redirects the user to the homepage
